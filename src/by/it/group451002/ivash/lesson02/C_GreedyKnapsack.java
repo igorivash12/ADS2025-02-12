c8721f4850c3116785634c5ceaadb5a1040a6c81
Игорь <igorivash12@gmail.com>
2025-03-31 14:25:07 +0300

    lesson02

---
+++ src/by/it/group451002/ivash/lesson02/C_GreedyKnapsack.java
@@ -0,0 +1,86 @@
+package by.it.group451002.ivash.lesson02;
+/*
+Даны
+1) объем рюкзака 4
+2) число возможных предметов 60
+3) сам набор предметов
+    100 50
+    120 30
+    100 50
+Все это указано в файле (by/it/a_khmelev/lesson02/greedyKnapsack.txt)
+
+Необходимо собрать наиболее дорогой вариант рюкзака для этого объема
+Предметы можно резать на кусочки (т.е. алгоритм будет жадным)
+ */
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class C_GreedyKnapsack {
+    public static void main(String[] args) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
+        InputStream inputStream = C_GreedyKnapsack.class.getResourceAsStream("greedyKnapsack.txt");
+        double costFinal = new C_GreedyKnapsack().calc(inputStream);
+        long finishTime = System.currentTimeMillis();
+        System.out.printf("Общая стоимость %f (время %d)", costFinal, finishTime - startTime);
+    }
+
+    double calc(InputStream inputStream) throws FileNotFoundException {
+        Scanner input = new Scanner(inputStream);
+        int n = input.nextInt();      //сколько предметов в файле
+        int W = input.nextInt();      //какой вес у рюкзака
+        Item[] items = new Item[n];   //получим список предметов
+        for (int i = 0; i < n; i++) { //создавая каждый конструктором
+            items[i] = new Item(input.nextInt(), input.nextInt());
+        }
+        //покажем предметы
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n", n, W);
+
+        Arrays.sort(items, Comparator.comparingDouble(Item::costPerWeight).reversed());
+
+        double result = 0.0;
+        int remainingWeight = W;
+
+        for (Item item : items) {
+            if (remainingWeight == 0) break; // Если рюкзак заполнен, выходим из цикла
+
+            if (item.weight <= remainingWeight) {
+                // Если предмет полностью помещается, добавляем его
+                result += item.cost;
+                remainingWeight -= item.weight;
+            } else {
+                // Если предмет не помещается, берем его часть
+                result += item.costPerWeight() * remainingWeight;
+                remainingWeight = 0;
+            }
+        }
+
+        System.out.printf("Удалось собрать рюкзак на сумму %f\n", result);
+        return result;
+    }
+
+    private static class Item {
+        int cost;
+        int weight;
+
+        Item(int cost, int weight) {
+            this.cost = cost;
+            this.weight = weight;
+        }
+
+        double costPerWeight() {
+            return (double) cost / weight; // Вычисляем стоимость за единицу веса
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Item{cost=%d, weight=%d}", cost, weight);
+        }
+    }
+}
